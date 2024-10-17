df6a62842b79bb73c531af19fe967263b10eb618
JD <591883039@qq.com>
2024-10-17 15:51:28 +0800

    feat: add componentConfig store

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
-import { useComponentsStore } from "../stores/components";
+import React, { useEffect } from "react";
+import { useComponentConfigStore } from "../stores/component-config";
+import { Component, useComponentsStore } from "../stores/components";
 
 export function EditArea() {
   const { components, addComponent, deleteComponent, updateComponentProps } =
     useComponentsStore();
+
+  const { componentConfig } = useComponentConfigStore();
   useEffect(() => {
     addComponent(
       {
@@ -18,24 +21,47 @@ export function EditArea() {
     addComponent(
       {
         id: 333,
-        name: "Video",
+        name: "Button",
         props: {},
         children: [],
       },
       222
     );
 
-    updateComponentProps(222, {
-      title: "6666",
-    });
+    // updateComponentProps(222, {
+    //   title: "6666",
+    // });
 
-    setTimeout(() => {
-      deleteComponent(333);
-    }, 3000);
+    // setTimeout(() => {
+    //   deleteComponent(333);
+    // }, 3000);
   }, []);
+
+  function renderComponents(components: Component[]): React.ReactNode {
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+
+      if (!config?.component) {
+        return null;
+      }
+
+      return React.createElement(
+        config.component,
+        {
+          key: component.id,
+          ...config.defaultProps,
+          ...component.props,
+        },
+        renderComponents(component.children || [])
+      );
+    });
+  }
+
   return (
-    <div>
-      <pre>{JSON.stringify(components, null, 2)}</pre>
+    <div className="h-[100%]">
+      {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
+
+      {renderComponents(components)}
     </div>
   );
 }
